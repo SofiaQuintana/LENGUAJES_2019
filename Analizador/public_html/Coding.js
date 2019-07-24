71822e775e6c36e908b1cee2335a55c7cbf30feb
Sofia Quintana <sofiaquintana2@gmail.com>
2019-07-23 22:43:39 -0600

    code final version

--- Analizador/public_html/Coding.js
+++ Analizador/public_html/Coding.js
@@ -6,6 +6,7 @@
 var inputText = "";
 var index = 0;
 var flag = 0;
+var isTrue = null;
 
 const charIndex = 0;
 const list = document.getElementById("list");
@@ -14,11 +15,12 @@ const text  = document.getElementById("inputBox");
 function lexicalAnalyzer() {
     receiveText();
     analyzeText(inputText);
+    document.getElementById("inputBox").value = "";
 }
 
 function analyzeText(text) {
-   var char = '';
-   var code = 0;
+    var char = '';
+    var code = 0;
    
        char = text.charAt(charIndex);
        code = char.charCodeAt(charIndex);
@@ -29,22 +31,17 @@ function analyzeText(text) {
            flag = 2;
        } else if (signsValidation(code)) {
            flag = 3;
-       }
-       
-        rules(flag, text);
-    
-    //addTable();
+       } 
+        rules(flag, text);    
 }
 
 function receiveText() {
     inputText = document.getElementById("inputBox").value;
-    alert(inputText);
 }
 
 function rules(flag, text) {
     var inputText = "";
     var charCode = 0;
-    var isTrue = null;
     
     inputText = text;
     switch(flag) {
@@ -52,25 +49,46 @@ function rules(flag, text) {
             for (i = 0; i < inputText.length; i++) {
                 charCode = inputText.charCodeAt(i);
                 if(letterValidation(charCode) || numberValidation(charCode)) {
-                    concatWord(inputText.charAt(i));
                     isTrue = true;
                 } else {
                     isTrue = false;
-//                    addTable("Error");
+                    break;
                 }
             } 
             verifyValidation(isTrue, "Identificador");
             break;
         case 2:
+            for (i = 0; i < inputText.length; i++) {
+                charCode = inputText.charCodeAt(i);
+                if(numberValidation(charCode)) {
+                    isTrue = true;
+                } else {
+                    isTrue = false;
+                    break;
+                }
+            }
+            verifyValidation(isTrue, "Numero");
             break;
         case 3:
+            for (i = 0; i < inputText.length; i++) {
+                charCode = inputText.charCodeAt(i);
+                if(signsValidation(charCode)) {
+                    isTrue = true;
+                } else {
+                    isTrue = false;
+                    break;
+                }
+            }
+            verifyValidation(isTrue, "Simbolo");
             break;
+        default:
+            isTrue = false;
+            verifyValidation(isTrue, "Error");    
     }
 }
 
 function addTable(token) {
     list.innerHTML = list.innerHTML + "<tr>" + "<td>" + text.value + "</td>" + "<td>" + token + "</td>" + "</tr>";
-    console.log(list);
 }
 
 function letterValidation(code) {
@@ -83,11 +101,6 @@ function numberValidation(code) {
     
 function signsValidation(code) {
     return (code >= 35 && code <= 37);
-}
-
-function concatWord(char) {
-    var inputText = "";
-    inputText += char;
 }
 
 function verifyValidation(flag, text) {
